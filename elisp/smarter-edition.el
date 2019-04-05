21311e4cef27b2a21174d531f3445196f13a30f1
Hugo Pompougnac <hugo.pompougnac@etu.univ-paris-diderot.fr>
2019-04-05 22:48:41 +0200

    move smarter-beginning-of-line

--- elisp/smarter-edition.el
+++ elisp/smarter-edition.el
@@ -1,29 +1,3 @@
-(defun smarter-beginning-of-line (arg)
-  
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  
-  (interactive "^p")
-  
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
 (defun smarter-kill-whitespaces ()
  (if (< (line-end-position) (point-max))
 	  (delete-region (point) (+ 1 (line-end-position)))
@@ -40,7 +14,6 @@ if empty."
       (smarter-kill-whitespaces)
     (kill-line)))
 
-;; Todo : restore column
 (defun smarter-kill-whole-line ()
 
   "Kills the current line preserving column position. Doesn't save newline
