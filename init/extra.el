650e987c0abc6ad3c8c8fabc30147e61dd5c9e36
hpompougnac <hugo.pompougnac@inria.fr>
2025-08-11 11:18:44 +0200

    bug: parentheses mismatch

--- init/extra.el
+++ init/extra.el
@@ -137,7 +137,7 @@
 (use-package markdown-mode
   :ensure t
   :config
-  (add-hook 'markdown-mode-hook #'markdown-toggle-url-hiding))
+  (add-hook 'markdown-mode-hook #'markdown-toggle-url-hiding)
   )
 
 (provide 'extra)
