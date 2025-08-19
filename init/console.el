b4951d52bf9e8b92711840aaa86ef23108d8212c
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-08-19 15:59:30 +0200

    vterm: fix colors

--- init/console.el
+++ init/console.el
@@ -1,11 +1,6 @@
 (use-package vterm
   :ensure t)
 
-;; (use-package vterm
-;;   :ensure t
-;;   :bind
-;;   ("C-x t" . vterm))
-
 (use-package multi-vterm
   :ensure t
   :bind
@@ -13,7 +8,7 @@
 
 (add-hook 'vterm-mode-hook
           (lambda ()
-            (hl-line-mode -1)
+            (face-remap-add-relative 'bold '(:weight normal))
             (when (bound-and-true-p global-hl-line-mode)
               (setq-local global-hl-line-mode nil))
             (define-key vterm-mode-map (kbd "C-<left>") 'windmove-left)
@@ -26,6 +21,27 @@
             (vterm-send-string "clear")
             (vterm-send-return)
             ))
+
+(with-eval-after-load 'vterm
+  (custom-set-faces
+   '(vterm-color-black   ((t (:foreground "gray25"      :background "gray25"))))
+   '(vterm-color-red     ((t (:foreground "red2"        :background "red2"))))
+   '(vterm-color-green   ((t (:foreground "green3"      :background "green3"))))
+   '(vterm-color-yellow  ((t (:foreground "goldenrod3"  :background "goldenrod3"))))
+   '(vterm-color-blue    ((t (:foreground "RoyalBlue2"  :background "RoyalBlue2"))))
+   '(vterm-color-magenta ((t (:foreground "purple2"     :background "purple2"))))
+   '(vterm-color-cyan    ((t (:foreground "cyan3"       :background "cyan3"))))
+   '(vterm-color-white   ((t (:foreground "gray97"      :background "gray97")))))
+
+  (dolist (pair '((vterm-color-bright-black   . vterm-color-black)
+                  (vterm-color-bright-red     . vterm-color-red)
+                  (vterm-color-bright-green   . vterm-color-green)
+                  (vterm-color-bright-yellow  . vterm-color-yellow)
+                  (vterm-color-bright-blue    . vterm-color-blue)
+                  (vterm-color-bright-magenta . vterm-color-magenta)
+                  (vterm-color-bright-cyan    . vterm-color-cyan)
+                  (vterm-color-bright-white   . vterm-color-white)))
+    (set-face-attribute (car pair) nil :inherit (cdr pair))))
 
 (add-hook 'vterm-mode-hook
           (lambda ()
