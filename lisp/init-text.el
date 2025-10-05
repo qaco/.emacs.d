334a7e6dd29cc5ee9f016ddbee61ef311027d53d
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-10-05 14:37:58 +0200

    text: create a dedicated file (#4)

---
+++ lisp/init-text.el
@@ -0,0 +1,18 @@
+(use-package yaml-mode
+  :ensure t)
+
+(use-package markdown-mode
+  :ensure t
+  :config
+  (add-hook 'markdown-mode-hook #'markdown-toggle-url-hiding)
+  )
+
+(use-package olivetti
+  :ensure t
+  :hook (text-mode . (lambda ()
+                       (unless (derived-mode-p 'yaml-mode)
+                         (olivetti-mode 1))))
+  :config
+  (setq olivetti-body-width 80))
+
+(provide 'init-text)
