3581583cb8ae2422b046b8aab2d3161b77d405a5
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-10-07 11:07:58 +0200

    use-package: make it available on older versions

--- init.el
+++ init.el
@@ -2,20 +2,26 @@
 (require 'use-package)
 (require 'package)
 
-(add-to-list
- 'package-archives
- '("melpa" . "https://melpa.org/packages/")
- t)
-
-(setq package-install-upgrade-built-in t)
-(package-initialize)
-
 (add-to-list 'display-buffer-alist
              '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
                (display-buffer-no-window)
                (allow-no-window . t)))
 
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/")
+ t)
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 (setq package-archive-enable-alist nil)
+
+(require 'use-package)
 
 (require 'init-text)
 (require 'init-ui)
