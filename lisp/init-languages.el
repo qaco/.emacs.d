98d55442e014a8c5b77e55b62fe0c46d1a7c4cb7
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-10-07 10:55:16 +0200

    eglot: out of standalone (#22)

--- lisp/init-languages.el
+++ lisp/init-languages.el
@@ -8,4 +8,20 @@
          (z3-smt2-mode . rainbow-delimiters-mode)
          (z3-mode . rainbow-delimiters-mode)))
 
+(use-package eglot
+  :ensure t
+  :hook ((python-mode . eglot-ensure)
+         (js-mode . eglot-ensure)
+         (go-mode . eglot-ensure)
+         (c-mode . eglot-ensure)
+         (c++-mode . eglot-ensure)
+         (rust-mode . eglot-ensure)
+         (mlir-mode . eglot-ensure)
+         )
+  :config
+  (add-to-list 'eglot-server-programs '(mlir-mode . ("mlir-lsp-server")))
+  (local-set-key (kbd "TAB") 'indent-for-tab-command)
+  (local-set-key (kbd "M-/") 'eglot-rename)
+  )
+
 (provide 'init-languages)
