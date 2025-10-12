5edf4eaa09ba64c58e5209e34b8fd046cd6b0624
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2025-10-12 22:58:42 +0200

    lang: less language features (#29)

--- lisp/init-languages.el
+++ lisp/init-languages.el
@@ -2,20 +2,11 @@
   :ensure t
   :mode ("\\`Snakefile\\'" . snakemake-mode))
 
-(use-package rainbow-delimiters
-  :ensure t
-  :hook ((emacs-lisp-mode . rainbow-delimiters-mode)
-         (z3-smt2-mode . rainbow-delimiters-mode)
-         (z3-mode . rainbow-delimiters-mode)))
-
 (use-package eglot
   :ensure t
   :hook ((python-mode . eglot-ensure)
-         (js-mode . eglot-ensure)
-         (go-mode . eglot-ensure)
          (c-mode . eglot-ensure)
          (c++-mode . eglot-ensure)
-         (rust-mode . eglot-ensure)
          (mlir-mode . eglot-ensure)
          )
   :config
